874a97d6a77bdeee3ba09ece0f9ea40fcd9092e9
OrangeMaxi <maximilianonaranjo@gmail.com>
2026-07-28 17:12:59 -0300

    Add model SQL for gold_resumen_franquicias view with totalPrice

--- models/views/gold_resumen_franquicias.sql
+++ models/views/gold_resumen_franquicias.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE VIEW bakehouse.gold_resumen_franquicias AS
 SELECT 
     franchiseID,
-    SUM(total_amount) AS ventas_totales,
+    SUM(totalPrice) AS ventas_totales,
     COUNT(transactionID) AS total_transacciones
 FROM bakehouse.sales_transactions
 GROUP BY franchiseID;
